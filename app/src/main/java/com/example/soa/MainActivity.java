667b3c3f51a01acb881a551511ff0a9971e3dde1
mitbapl <mitba.prashant@gmail.com>
2025-06-21 12:35:58 +0530

    done

---
+++ app/src/main/java/com/example/soa/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.soa;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView output = findViewById(R.id.output);
+        final Button extract = findViewById(R.id.extractBtn);
+
+        extract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                output.setText("SOA Analyzer is working!");
+            }
+        });
+    }
+}
